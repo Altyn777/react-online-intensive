470bf8780d26dbc916c6faa83b36fc9680888fe1
Altyn777 <darzeeia@gmail.com>
2019-08-25 18:24:04 +0300

    lesson-5

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,17 +5,37 @@ import React, {Component} from 'react';
 import StatusBar from '../StatusBar';
 import Composer from '../Composer';
 import Post from '../Post';
+import Spinner from '../Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '123', comment: 'Hi there!', created: 1526825076849 },
+            { id: '456', comment: 'Привет!', created: 1526825076855 },
+        ], // свойство, литерал массива
+    };
+
     render() {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => { // экземпляр класса posts будет создан каждый раз при создании поста, рендерим список постов с пом. map
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            ); // возвращаем по экземлпяру компонента post
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
